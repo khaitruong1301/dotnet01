dd56f8e4d8042c672a82e0c4569a9657fce12153
khai <khaitruong1301@gmail.com>
2024-12-18 14:28:08 +0700

    buoi2_handle_event_2waybinding

---
+++ web_blazor/ViewModels/UserViewModel.cs
@@ -0,0 +1,17 @@
+namespace web_blazor.ViewModels;
+
+public class UserViewModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string FullName { get; set; }
+    public string Gender {get;set;}
+    public string Country{get;set;}
+
+    // public Category cate = new Category();
+}
+
+// public class Category {
+//     public string id {get;set;}
+//     public string name {get;set;}
+// }
--- web_blazor/obj/Debug/netcoreapp8.0/web_blazor.AssemblyInfo.cs
+++ web_blazor/obj/Debug/netcoreapp8.0/web_blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1b3ecc58aab16669810b1054d79e3a21e885f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14594fbf82a7708feea1deb9f97a6c17da74309")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
