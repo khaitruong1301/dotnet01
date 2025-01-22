bb0915550b0fc00b1a48476a769468f5d2518def
khai <khaitruong1301@gmail.com>
2025-01-22 15:05:00 +0700

    dp

--- web_blazor/obj/Debug/netcoreapp8.0/web_blazor.AssemblyInfo.cs
+++ web_blazor/obj/Debug/netcoreapp8.0/web_blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273d0ea5f042403d82ab66892484a21b52a05eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b7a6cd7f922f64214e60a56fbc66924461194d")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
